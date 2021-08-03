6c5c5a62cd7e3424eb756e7df223bce2db704a8f
charliecog <charlie.coggans@io-academy.uk>
2021-08-03 14:38:59 +0100

    confirm read warning before logging in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ProvideAuth } from './Hooks/useAuth';
 import Routes from './Routes/Routes';
 
--- src/Components/Atoms/LoginButton/LoginButton.js
+++ src/Components/Atoms/LoginButton/LoginButton.js
@@ -3,6 +3,7 @@ import { useAuth } from '../../../Hooks/useAuth';
 
 const LoginButton = () => {
     const [email, setEmail] = useState('');
+    const [termsUnderstood, setTermsUnderstood] = useState(false);
     const auth = useAuth();
     return (
         <>
@@ -10,7 +11,13 @@ const LoginButton = () => {
                 <>
                     <h4>Please enter your email address</h4>
                     <input type="text" value={email} onInput={(e) => setEmail(e.target.value)} />
-                    <button onClick={() => auth.signin(email)}>Login</button>
+                    <button disabled={!termsUnderstood} onClick={() => auth.signin(email)}>
+                        Login
+                    </button>
+                    <div>
+                        Please check the box to confirm that you have read and understood the warning message
+                        <input type="checkbox" onChange={() => setTermsUnderstood(!termsUnderstood)} />
+                    </div>
                 </>
             ) : (
                 <button onClick={() => auth.signout()}>Logout</button>
