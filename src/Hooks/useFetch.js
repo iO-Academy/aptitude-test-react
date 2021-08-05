20e42cfafa52bed3742f3e5f4506a6c1a427cdf8
charliecog <charlie.coggans@io-academy.uk>
2021-08-05 11:44:14 +0100

    adds form data utility to transform post data for the api

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,4 +1,9 @@
+import jsonToFormData from '../Components/Utilities/formData';
+
 const fetchApi = async (url, options = {}) => {
+    if ('method' in options && options.method === 'POST') {
+        options.body = jsonToFormData(options.body);
+    }
     let response = await fetch(process.env.REACT_APP_BASE_URL + url, options);
     let data = await response.json();
     return data;
