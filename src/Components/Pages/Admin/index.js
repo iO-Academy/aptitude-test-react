3858f5aa3bf7a79e3c504bd1e52968077b574d7e
jesshamby <jesshamby99@gmail.com>
2021-10-25 16:49:15 +0100

    Did a fetch to the question section of the API and stored in questions state hwoever this only collects questions with a test id of 1

--- src/Components/Pages/Admin/index.js
+++ src/Components/Pages/Admin/index.js
@@ -10,12 +10,15 @@ const Admin = () => {
     const [users, setUsers] = useState(null);
     //initial state of the table is null until users is populated
     const [table, setTable] = useState(null);
+    //initial state of the questions for test1
+    const [questions, setQuestions] = useState(null);
 
     // the use effect hook is passed a 2nd param of [], ensuring it only runs once when the component is first mounted
     useEffect(async () => {
         // common error: the code calling the API is often placed directly in here - abstracting it into its own
         // function (getUsers) is key to this pattern working
         await getUsers();
+        await getQuestions();
     }, []);
 
     // because it is abstracted here we have control when it occurs - once when component first mounted then whenever
@@ -26,6 +29,14 @@ const Admin = () => {
             return setUsers(response.data);
         }
     };
+
+    const getQuestions = async () => {
+        let response = await fetchApi(`question`);
+        if (response.success) {
+            return setQuestions(response.data);
+        }
+    };
+    console.log(questions);
 
     useEffect(() => {
         users ? setTable(<UserTable users={users} />) : '';
