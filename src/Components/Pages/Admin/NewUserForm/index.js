bcc790932e4d4b2985bc81f82f4dd28c88c92dd4
Tom Garrett <85767932+tomgarrett95@users.noreply.github.com>
2021-10-25 14:06:48 +0100

    outline for form index.js

---
+++ src/Components/Pages/Admin/NewUserForm/index.js
@@ -0,0 +1,24 @@
+const NewUserHeading = () => {
+    return <h2>Add new user</h2>;
+};
+
+const NewUserInput = () => {
+
+}
+
+const NewUserSubmit = () => {
+
+}
+
+
+const NewUserForm = () => {
+    return (
+        <div>
+            <NewUserHeading />
+            <NewUserInput />
+            <NewUserSubmit />
+        </div>
+    );
+};
+
+export default NewUserForm;
