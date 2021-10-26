1df0b385a348b4b5b0cffbb67ce05eecf72050f2
jesshamby <jesshamby99@gmail.com>
2021-10-26 13:49:15 +0100

    added in a score calculator

--- src/Components/Pages/Admin/index.js
+++ src/Components/Pages/Admin/index.js
@@ -10,20 +10,13 @@ const Admin = () => {
     const [users, setUsers] = useState(null);
     //initial state of the table is null until users is populated
     const [table, setTable] = useState(null);
-    //initial state of the tests is null until tests is populated
-    const [tests, setTest] = useState(null);
 
     // the use effect hook is passed a 2nd param of [], ensuring it only runs once when the component is first mounted
     useEffect(async () => {
         // common error: the code calling the API is often placed directly in here - abstracting it into its own
         // function (getUsers) is key to this pattern working
         await getUsers();
-        await getTests();
     }, []);
-
-    useEffect(() => {
-        tests ? getQuestions(tests) : '';
-    }, [tests]);
 
     // because it is abstracted here we have control when it occurs - once when component first mounted then whenever
     // we choose from that point onwards, thus no infinite recalling.
@@ -33,26 +26,6 @@ const Admin = () => {
             return setUsers(response.data);
         }
     };
-
-    const getTests = async () => {
-        let response = await fetchApi(`test`);
-        if (response.success) {
-            return setTest(response.data);
-        }
-    };
-
-    const getQuestions = (tests) => {
-        if (tests) {
-            tests.forEach(async (test) => {
-                let response = await fetchApi(`question?test_id=${test.id}`);
-                if (response.success) {
-                    totalScores.push(response.data.length);
-                }
-            });
-        }
-    };
-
-    let totalScores = [];
 
     useEffect(() => {
         users ? setTable(<UserTable users={users} />) : '';
