64313973d181e2de7b48bdc38b2872cab8373fac
Jake Rooms <jakeprooms@outlook.com>
2021-10-26 14:04:52 +0100

    changed comments on useJoin hook

--- src/Hooks/useJoin.js
+++ src/Hooks/useJoin.js
@@ -9,17 +9,15 @@
  * @returns {*}
  */
 const useJoin = ([data1, data1key1], [data2, data2key1, data2key2]) => {
-    // maps the users array to a temporary array with the value in user.test_id replaced with the name from the corresponding test
-    let newData1 = data1.map((data1Values) => {
-        // filters the tests array based on if the test id matches the data1Values test id
+    // maps the data1 array to an array with the value in data1.data1key1 replaced with the value with key data2key2 from the corresponding data2key1
+    return data1.map((data1Values) => {
+        // filters the data2 array based on if data2key1 matches the data1Values data1key1
         let filteredData2 = data2.filter((data2Values) => {
             return data2Values[data2key1] === data1Values[data1key1];
         });
         data1Values[data1key1] = filteredData2[0][data2key2];
         return data1Values;
     });
-    // sets the array constructed in newData1 to the variable newUsers
-    return newData1;
 };
 
 export default useJoin;
