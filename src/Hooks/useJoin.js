146fc737bdc87b06d1d63a52d26495e23eba5572
Jake Rooms <jakeprooms@outlook.com>
2021-10-26 14:01:12 +0100

    added docblock to useJoin

--- src/Hooks/useJoin.js
+++ src/Hooks/useJoin.js
@@ -1,3 +1,13 @@
+/**
+ * performs a left join from data 2 onto data 1 based off the keys
+ *
+ * @param data1 set of data from the fetchAPI that we want to join on to
+ * @param data1key1 the key we want to map on to
+ * @param data2 set of data from the fetchAPI that we want to join in to data1
+ * @param data2key1 the key we're mapping on to data1key1
+ * @param data2key2 the key we're joining the value data in from.
+ * @returns {*}
+ */
 const useJoin = ([data1, data1key1], [data2, data2key1, data2key2]) => {
     // maps the users array to a temporary array with the value in user.test_id replaced with the name from the corresponding test
     let newData1 = data1.map((data1Values) => {
