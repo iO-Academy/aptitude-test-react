73cc2adb9152b71aaee59032fd6f77986cca176d
kimberley <kim.dobney@yahoo.co.uk>
2021-10-26 12:02:10 +0100

    find function created, grabs question based on currentquestionid

--- src/Components/Organisms/Test/Test.js
+++ src/Components/Organisms/Test/Test.js
@@ -4,10 +4,18 @@ import fetchApi from '../../../Hooks/useFetch';
 const Test = () => {
     const [questions, setQuestions] = useState([]);
     const [currentQuestionId, setCurrentQuestionId] = useState(1);
+    const [currentQuestion, setCurrentQuestion] = useState([]);
     useEffect(async () => {
         let data = await fetchApi('/question');
         setQuestions(data.data);
     }, []);
+    function find(currentQuestionId, setCurrentQuestion) {
+        useEffect(async () => {
+            let res = await fetchApi('/question/' + currentQuestionId);
+            setCurrentQuestion(res.data);
+        }, [currentQuestionId]);
+    }
+    find(currentQuestionId, setCurrentQuestion);
     return (
         <>
             <p>Hello Hello Testing Testing Quiz Time</p>
