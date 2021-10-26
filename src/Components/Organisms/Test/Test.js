c02117a82a8423b94d65806b23a190bb4cf4aa38
mclack <miloclack@hotmail.co.uk>
2021-10-26 16:18:12 +0100

    updateUserAnswers function created. Will update and add to the userAnswers object in state

--- src/Components/Organisms/Test/Test.js
+++ src/Components/Organisms/Test/Test.js
@@ -2,13 +2,24 @@ import { useEffect, useState } from 'react';
 import fetchApi from '../../../Hooks/useFetch';
 
 const Test = () => {
+    const [userAnswers, setUserAnswers] = useState({});
     const [questions, setQuestions] = useState([]);
     const [currentQuestionId, setCurrentQuestionId] = useState(1);
     const [currentQuestion, setCurrentQuestion] = useState([]);
+
     useEffect(async () => {
         let data = await fetchApi('/question');
         setQuestions(data.data);
     }, []);
+
+    function updateUserAnswers(currentQuestionId, userAnswer) {
+        if (userAnswer >= 1 && userAnswer <= 5 && validateTestLength(currentQuestionId)) {
+            let currentAnswers = userAnswers;
+            currentAnswers[currentQuestionId] = userAnswer;
+            setUserAnswers(currentAnswers);
+        }
+    }
+
     function find(currentQuestionId, setCurrentQuestion) {
         useEffect(async () => {
             let res = await fetchApi('/question/' + currentQuestionId);
@@ -16,19 +27,32 @@ const Test = () => {
             return currentQuestion;
         }, [currentQuestionId]);
     }
+
+    function validateTestLength(targetQuestionId) {
+        let numberOfQuestions = questions.length;
+        if (targetQuestionId > 0 && targetQuestionId <= numberOfQuestions) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     find(currentQuestionId, setCurrentQuestion);
     const modifyQuestionId = (targetQuestionId) => {
-        let numberOfQuestions = questions.length;
-
-        if (targetQuestionId > 0 && targetQuestionId <= numberOfQuestions) {
+        // numberOfQuestions moved to global scope for re-use in updateUserAnswers function
+        // If statement turned into a function for validation re-use in updateUserAnswers function
+        if (validateTestLength(targetQuestionId)) {
             setCurrentQuestionId(targetQuestionId);
         }
     };
+
+    console.log(userAnswers);
     return (
         <>
             <p>{currentQuestion.text}</p>
             <p onClick={() => modifyQuestionId(3)}>Hello Hello Testing Testing Quiz Time</p>
             <p>{currentQuestionId}</p>
+            <p onClick={() => updateUserAnswers(4, 5)}>updateUserAnswers test</p>
         </>
     );
 };
